61b0eecc2c2667c2344f20cc967114082148a814
Kumma30mvp <130480550+Kumma30mvp@users.noreply.github.com>
2024-01-16 11:30:59 -0500

    Ex1_func
    Funciona

--- maxNumberOfBalloons.cpp
+++ maxNumberOfBalloons.cpp
@@ -5,10 +5,25 @@ using namespace std;
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-		// add your code 
-		
-		return 0;
-    }
+		int b = 0, a = 0, l = 0, o = 0, n = 0;
+		for (char c : text){
+			if (c == 'b') b++;
+			else if (c == 'a') a++;
+			else if (c == 'l') l++;
+			else if (c == 'o') o++;
+			else if (c == 'n') n++;
+		}
+		int ans = 0;
+		while (b >= 1 && a >= 1 && l >= 2 && o >= 2 && n >= 1){
+			ans++;
+			b--;
+			a--;
+			l -= 2;
+			o -= 2;
+			n--;
+		}
+		return ans;
+    };
 };
 
 void test(){
